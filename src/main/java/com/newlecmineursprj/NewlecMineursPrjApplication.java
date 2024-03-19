57825b90ee0ad2a76d03870b199ca71f2c4f1535
박철현 <tghy4@naver.com>
2024-03-19 11:21:21 +0900

    초기설정 완료

---
+++ src/main/java/com/newlecmineursprj/NewlecMineursPrjApplication.java
@@ -0,0 +1,11 @@
+package com.newlecmineursprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlecMineursPrjApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewlecMineursPrjApplication.class, args);
+    }
+}
---
+++ src/test/java/com/newlecminursprj/NewlecMineursPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlecminursprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewlecMineursPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
