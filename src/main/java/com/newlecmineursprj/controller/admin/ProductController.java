36c9a813d47a1274b62f3d2e57bb53df5801c659
tjfwltn <tjfwltn1234@gmail.com>
2024-03-25 15:06:17 +0900

    잘되네요

--- src/main/java/com/newlecmineursprj/controller/admin/ProductController.java
+++ src/main/java/com/newlecmineursprj/controller/admin/ProductController.java
@@ -24,6 +24,7 @@ public class ProductController {
         log.info("list: {}", list);
         model.addAttribute("list", list);
         return "admin/products/list";
+        // 잘되나?
     }
 
     @GetMapping("{id}")
@@ -35,8 +36,8 @@ public class ProductController {
 
     // @PostMapping
     // public String reg(@ModelAttribute ProductEntity product) {
-    //     System.out.println("entity = " + product);
-    //     return "redirect:/admin/products";
+    // System.out.println("entity = " + product);
+    // return "redirect:/admin/products";
     // }
     @GetMapping("/reg")
     public String regForm() {
