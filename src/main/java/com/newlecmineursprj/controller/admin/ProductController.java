b2194de5a7a131ba936543626758539cce7e4ce7
tjfwltn <tjfwltn1234@gmail.com>
2024-03-28 16:35:18 +0900

    feat:edit 페이지 이미지 미리보기 구현

--- src/main/java/com/newlecmineursprj/controller/admin/ProductController.java
+++ src/main/java/com/newlecmineursprj/controller/admin/ProductController.java
@@ -2,15 +2,17 @@ package com.newlecmineursprj.controller.admin;
 
 import java.util.List;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.newlecmineursprj.entity.Category;
 import com.newlecmineursprj.entity.Product;
@@ -55,7 +57,6 @@ public class ProductController {
     public String edit(@RequestBody Product product) {
         System.out.println("hi");
         service.edit(product);
-
         return "success";
     }
 
