e094be626b9a88c2c537d521803d50ee71dc70f8
👻👽🤖🧙‍♂️🫠 <sprain6628@gmail.com>
2024-04-11 17:11:50 +0900

    feat: 테마 토글 js 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/main/resources/templates/**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui"),
-    require('@tailwindcss/typography')],
-  daisyui: {
-    themes: ["light", "dark", "cupcake"],
-  },
+    content: ["./src/main/resources/templates/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("daisyui"),
+        require('@tailwindcss/typography')],
+    daisyui: {
+        themes: ["light", "dark", "cupcake"],
+    },
 }
 
