aaca0571e6775737c340e9ee1036547d0645bef5
holymolyhi <cxop1756@naver.com>
2024-04-18 18:21:10 +0900

    feat: 쿠폰 내역 페이지 구현중 # 1

--- src/main/java/com/newlecmineursprj/entity/Coupon.java
+++ src/main/java/com/newlecmineursprj/entity/Coupon.java
@@ -2,9 +2,15 @@ package com.newlecmineursprj.entity;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Coupon {
     private long id;
     private String name;
