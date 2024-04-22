14ccca268467756f9a776ccbd25d616f4baa082c
JOON <alwayswith15@gmail.com>
2024-04-23 01:04:45 +0900

    유저페이지#2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     content: ["./src/main/resources/templates/**/*.html"],
     theme: {
         extend: {},
+        container: {
+            center: true,
+            padding: '1rem',
+            screens: {
+                sm: '100%',
+                md: '100%',
+                lg: '1600px',
+                xl: '2220px',
+            },
+        },
     },
     plugins: [
         require("daisyui"),
