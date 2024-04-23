d14c1b68671b528ef339cd966e24291833a57916
JOON <alwayswith15@gmail.com>
2024-04-23 16:20:28 +0900

    관리자페이지#3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,7 @@ module.exports = {
     content: ["./src/main/resources/templates/**/*.html"],
     theme: {
         extend: {},
-        container: {
-            center: true,
-            padding: '1rem',
-            screens: {
-                sm: '100%',
-                md: '100%',
-                lg: '1600px',
-                xl: '2220px',
-            },
-        },
+
     },
     plugins: [
         require("daisyui"),
