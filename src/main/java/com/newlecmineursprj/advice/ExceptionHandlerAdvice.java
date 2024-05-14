d3ab7c6aed0795e5b09bfffd623bedbea66ae48a
박철현 <tghy4@naver.com>
2024-05-14 11:58:41 +0900

    feat:ErrorController Advice 구현

--- src/main/java/com/newlecmineursprj/advice/ExceptionHandlerAdvice.java
+++ src/main/java/com/newlecmineursprj/advice/ExceptionHandlerAdvice.java
@@ -2,10 +2,11 @@ package com.newlecmineursprj.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 
 @Slf4j
@@ -13,16 +14,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class ExceptionHandlerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public ModelAndView numberFormatExHandler(Exception ex1) {
-        log.error("Exception = {}",ex1);
-        log.error("Exception Message = {}",ex1.getMessage());
-
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("error", ex1.getMessage());
-        mv.addObject("errorCode", HttpStatus.BAD_REQUEST.value());
-        mv.setViewName("/error/404");
-        return mv;
+    @ExceptionHandler
+    public String noResourceFoundExHandler(NoResourceFoundException ex, Model model) {
+        ErrorResult result = new ErrorResult(
+                ex,"잘못된 페이지 요청입니다.",
+                HttpStatus.BAD_REQUEST.value(),"/error/4xx"
+        );
+        model.addAttribute("error", result);
+        return result.getViewName();
     }
 }
 
