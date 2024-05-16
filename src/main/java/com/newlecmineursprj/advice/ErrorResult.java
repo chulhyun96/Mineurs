a3b0aca6e4b8f7a3ceee17385227f944b60b2ef4
박철현 <tghy4@naver.com>
2024-05-16 14:44:59 +0900

    refact:ErrorResult JavaDoc 수정

--- src/main/java/com/newlecmineursprj/advice/ErrorResult.java
+++ src/main/java/com/newlecmineursprj/advice/ErrorResult.java
@@ -13,6 +13,7 @@ public class ErrorResult extends Exception{
     private String viewName;
     /**
      * 생성자 메서드를 오버라이딩해서 사용가능
+     * null 및 불필요한 인자 넣어서 생성하지 않기
      */
 
     public ErrorResult(Exception exception, String defaultMessage, String extendMessage, Integer code, String viewName) {
