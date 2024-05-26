7e98c97507f027d647599ff57e1178a2a2b7eb10
tjfwltn <tjfwltn1234@gmail.com>
2024-05-26 19:20:27 +0900

    #2

--- src/main/java/com/newlecmineursprj/NewlecMineursPrjApplication.java
+++ src/main/java/com/newlecmineursprj/NewlecMineursPrjApplication.java
@@ -2,6 +2,7 @@ package com.newlecmineursprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertyResolver;
@@ -10,6 +11,9 @@ import java.util.Properties;
 
 @SpringBootApplication
 public class NewlecMineursPrjApplication extends SpringBootServletInitializer {
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NewlecMineursPrjApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(NewlecMineursPrjApplication.class, args);
     }
